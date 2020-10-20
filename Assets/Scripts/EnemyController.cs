fed3eacdd75d0eee6a9574b1372eafc89869704e
Devlin McClure <mccluredt@gmail.com>
2020-10-20 19:25:52 -0400

    Create Enemy that follows player

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(player.transform.position);
+    }
+}
