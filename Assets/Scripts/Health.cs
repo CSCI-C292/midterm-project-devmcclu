6f0078cd1bd464c6d6d3a759919e94165323ef58
Devlin McClure <mccluredt@gmail.com>
2020-10-20 19:25:32 -0400

    Create Health script, attach to Player prefab

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] int health = 100;
+    [SerializeField] int armor = 0;
+
+    public void TakeDamge(int value)
+    {
+        if (armor > 0)
+        {
+            health -= value / 2;
+            armor -= value / 2;
+        }
+        else
+        {
+            health -= value;
+        }
+    }
+}
