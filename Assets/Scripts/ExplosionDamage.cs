a9e44308d1f596fa76bd2b49c53975d5ed16c65e
Devlin McClure <mccluredt@gmail.com>
2020-12-02 19:21:12 -0500

    Create Rocket Launcher explosion, fix gravity on ammo

---
+++ Assets/Scripts/ExplosionDamage.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionDamage : MonoBehaviour
+{
+    
+    [SerializeField] int _damage = 20;
+    [SerializeField] float _timer = 2f;
+
+    void Update()
+    {
+        if(_timer <= 0)
+        {
+            Destroy(gameObject);
+        }    
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        if(other.gameObject.tag.Equals("Player") || other.gameObject.tag.Equals("Enemy"))
+        {
+            other.gameObject.GetComponent<Health>().TakeDamge(_damage);
+        }
+    }
+}
---
+++ Assets/Scripts/RocketExplosion.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketExplosion : MonoBehaviour
+{
+    [SerializeField] GameObject _explosion;
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag.Equals("Enemy"))
+        {
+            Instantiate(_explosion, transform.position, Quaternion.identity);        }
+    }
+}
